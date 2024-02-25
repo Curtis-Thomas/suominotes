b1e30d3bc538ef5530910da152d0fd9943077da4
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-02-25 09:42:05 +0200

    add: main info txt + github star link

--- src/core/Main.tsx
+++ src/core/Main.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import BtnRouting from "../components/navigation/navigationBtn/BtnRouting";
+import GitHubButton from "react-github-btn";
 
 function Main() {
   return (
@@ -10,6 +11,35 @@ function Main() {
         margin: "auto",
       }}
     >
+      <Typography sx={{ textAlign: "center" }}>
+        Free open-source Finnish language platform.
+      </Typography>
+      <Typography></Typography>
+      <Typography sx={{ textAlign: "center", pt: 1, pb: 2 }}>
+        If you have found this platform useful, please consider starring the
+        project on GitHub.
+      </Typography>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          pb: 2,
+        }}
+      >
+        <GitHubButton
+          href="https://github.com/Curtis-Thomas/suominotes"
+          data-icon="octicon-star"
+          aria-label="Star buttons/github-buttons on GitHub"
+          data-show-count="true"
+          data-size="large"
+        >
+          Star
+        </GitHubButton>
+      </Box>
+
       <BtnRouting btnName="English" navigateTo="/englishDash" />
     </Box>
   );
