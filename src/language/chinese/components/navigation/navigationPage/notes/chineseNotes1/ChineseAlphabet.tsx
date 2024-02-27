1785c2c91855eee2d5e9e3ea320ac4a91d3eab7f
Hope <yagamidai@gmail.com>
2024-02-27 17:12:54 +0800

    translate note1 - alphabet into Chinese

--- src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseAlphabet.tsx
+++ src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseAlphabet.tsx
@@ -7,36 +7,36 @@ function ChineseAlphabet() {
         Alphabet
       </Typography>
       <Box>
-        <Typography>A - auto, aamu / car, morning</Typography>
-        <Typography>B - bananni, bussi / bananna, bus</Typography>
-        <Typography>C - CD-leevy / CD disc</Typography>
+        <Typography>A - auto, aamu / car, morning / 汽车，早餐</Typography>
+        <Typography>B - bananni, bussi / bananna, bus / 香蕉，公交车</Typography>
+        <Typography>C - CD-leevy / CD disc / CD光盘</Typography>
         <Typography>
-          D - delfinni, dekkari / dolphin, detective novel
+          D - delfinni, dekkari / dolphin, detective novel / 海豚，侦探小说
         </Typography>
-        <Typography>E - elämi, ei, eilen / life, no, yesterday </Typography>
-        <Typography>F - farkut, fysiikka / jeans, physics</Typography>
-        <Typography>G - grilli, gerbilli / barbeque, gerbile</Typography>
-        <Typography>H - hei, hyvä, helppo / hello, good, easy</Typography>
-        <Typography>I - iloinen ihminen / happy person</Typography>
-        <Typography>J - joulu, juusto / Christmas, cheese</Typography>
-        <Typography>K - koulu, kaikki / school, everyone</Typography>
-        <Typography>L - luonto, laki / nature, law</Typography>
-        <Typography>M - minä, me, maa / I, we, country</Typography>
-        <Typography>O - oppia, oppilas / to learn, student</Typography>
-        <Typography>P - paras, piano / the best, piano</Typography>
-        <Typography>Q - Qatar / Qatar</Typography>
-        <Typography>R - ruoka, ruisleipä / food, rye bread</Typography>
-        <Typography>S - sinä, Suomi / you, Finland</Typography>
-        <Typography>T - tie, tietää, te / way, to know, you(plural)</Typography>
-        <Typography>U - uusi, unelma / new, dream</Typography>
-        <Typography>V - vitsi, vanha / joke, old</Typography>
-        <Typography>W - WC = vessa / WC</Typography>
-        <Typography>X - xylofoni / xylophone</Typography>
-        <Typography>Y - yksi, yrittää / one, to try</Typography>
-        <Typography>Z - zen, zen</Typography>
-        <Typography>Å - Åbo / Turku</Typography>
-        <Typography>Ä - ääni, älykäs / sound, intelligent</Typography>
-        <Typography>Ö - ötökkä, öljy / bug, oil</Typography>
+        <Typography>E - elämi, ei, eilen / life, no, yesterday / 生命，不，昨天</Typography>
+        <Typography>F - farkut, fysiikka / jeans, physics / 牛仔裤，物理</Typography>
+        <Typography>G - grilli, gerbilli / barbeque, gerbile / 烧烤，沙鼠</Typography>
+        <Typography>H - hei, hyvä, helppo / hello, good, easy / 你好，好，容易</Typography>
+        <Typography>I - iloinen ihminen / happy person / 快乐的人</Typography>
+        <Typography>J - joulu, juusto / Christmas, cheese / 圣诞节，芝士</Typography>
+        <Typography>K - koulu, kaikki / school, everyone / 学校，每个人</Typography>
+        <Typography>L - luonto, laki / nature, law / 自然，法律</Typography>
+        <Typography>M - minä, me, maa / I, we, country / 我，我们，国家</Typography>
+        <Typography>O - oppia, oppilas / to learn, student / 学习，学生</Typography>
+        <Typography>P - paras, piano / the best, piano / 最好的，钢琴</Typography>
+        <Typography>Q - Qatar / Qatar / 卡塔尔</Typography>
+        <Typography>R - ruoka, ruisleipä / food, rye bread / 食物，黑麦面包 </Typography>
+        <Typography>S - sinä, Suomi / you, Finland / 你，芬兰</Typography>
+        <Typography>T - tie, tietää, te / way, to know, you(plural) / 路，了解，你们</Typography>
+        <Typography>U - uusi, unelma / new, dream / 新，梦</Typography>
+        <Typography>V - vitsi, vanha / joke, old / 玩笑，老</Typography>
+        <Typography>W - WC = vessa / WC / 厕所</Typography>
+        <Typography>X - xylofoni / xylophone / 木琴</Typography>
+        <Typography>Y - yksi, yrittää / one, to try / 一，试</Typography>
+        <Typography>Z - zen / zen / 禅</Typography>
+        <Typography>Å - Åbo / Turku / 图尔库 (Åbo为其瑞典语称呼) </Typography>
+        <Typography>Ä - ääni, älykäs / sound, intelligent / 声音，智慧</Typography>
+        <Typography>Ö - ötökkä, öljy / bug, oil / 虫子，油</Typography>
       </Box>
     </Box>
   );
