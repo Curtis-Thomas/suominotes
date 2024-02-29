1b48c38632b02339707815e707e7ecde89c3588d
Hope <yagamidai@gmail.com>
2024-02-29 15:21:19 +0800

    deleted the English part of content

--- src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseAlphabet.tsx
+++ src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseAlphabet.tsx
@@ -7,36 +7,36 @@ function ChineseAlphabet() {
         Alphabet
       </Typography>
       <Box>
-        <Typography>A - auto, aamu / car, morning / 汽车，早餐</Typography>
-        <Typography>B - bananni, bussi / bananna, bus / 香蕉，公交车</Typography>
-        <Typography>C - CD-leevy / CD disc / CD光盘</Typography>
+        <Typography>A - auto, aamu / 汽车，早餐</Typography>
+        <Typography>B - bananni, bussi / 香蕉，公交车</Typography>
+        <Typography>C - CD-leevy / CD光盘</Typography>
         <Typography>
-          D - delfinni, dekkari / dolphin, detective novel / 海豚，侦探小说
+          D - delfinni, dekkari / 海豚，侦探小说
         </Typography>
-        <Typography>E - elämi, ei, eilen / life, no, yesterday / 生命，不，昨天</Typography>
-        <Typography>F - farkut, fysiikka / jeans, physics / 牛仔裤，物理</Typography>
-        <Typography>G - grilli, gerbilli / barbeque, gerbile / 烧烤，沙鼠</Typography>
-        <Typography>H - hei, hyvä, helppo / hello, good, easy / 你好，好，容易</Typography>
-        <Typography>I - iloinen ihminen / happy person / 快乐的人</Typography>
-        <Typography>J - joulu, juusto / Christmas, cheese / 圣诞节，芝士</Typography>
-        <Typography>K - koulu, kaikki / school, everyone / 学校，每个人</Typography>
-        <Typography>L - luonto, laki / nature, law / 自然，法律</Typography>
-        <Typography>M - minä, me, maa / I, we, country / 我，我们，国家</Typography>
-        <Typography>O - oppia, oppilas / to learn, student / 学习，学生</Typography>
-        <Typography>P - paras, piano / the best, piano / 最好的，钢琴</Typography>
-        <Typography>Q - Qatar / Qatar / 卡塔尔</Typography>
-        <Typography>R - ruoka, ruisleipä / food, rye bread / 食物，黑麦面包 </Typography>
-        <Typography>S - sinä, Suomi / you, Finland / 你，芬兰</Typography>
-        <Typography>T - tie, tietää, te / way, to know, you(plural) / 路，了解，你们</Typography>
-        <Typography>U - uusi, unelma / new, dream / 新，梦</Typography>
-        <Typography>V - vitsi, vanha / joke, old / 玩笑，老</Typography>
-        <Typography>W - WC = vessa / WC / 厕所</Typography>
-        <Typography>X - xylofoni / xylophone / 木琴</Typography>
-        <Typography>Y - yksi, yrittää / one, to try / 一，试</Typography>
-        <Typography>Z - zen / zen / 禅</Typography>
+        <Typography>E - elämi, ei, eilen / 生命，不，昨天</Typography>
+        <Typography>F - farkut, fysiikka / 牛仔裤，物理</Typography>
+        <Typography>G - grilli, gerbilli / 烧烤，沙鼠</Typography>
+        <Typography>H - hei, hyvä, helppo / 你好，好，容易</Typography>
+        <Typography>I - iloinen ihminen / 快乐的人</Typography>
+        <Typography>J - joulu, juusto / 圣诞节，芝士</Typography>
+        <Typography>K - koulu, kaikki / 学校，每个人</Typography>
+        <Typography>L - luonto, laki / 自然，法律</Typography>
+        <Typography>M - minä, me, maa / 我，我们，国家</Typography>
+        <Typography>O - oppia, oppilas / 学习，学生</Typography>
+        <Typography>P - paras, piano / 最好的，钢琴</Typography>
+        <Typography>Q - Qatar / 卡塔尔</Typography>
+        <Typography>R - ruoka, ruisleipä / 食物，黑麦面包 </Typography>
+        <Typography>S - sinä, Suomi / 你，芬兰</Typography>
+        <Typography>T - tie, tietää, te / 路，了解，你们</Typography>
+        <Typography>U - uusi, unelma / 新，梦</Typography>
+        <Typography>V - vitsi, vanha / 玩笑，老</Typography>
+        <Typography>W - WC = vessa / 厕所(WC)</Typography>
+        <Typography>X - xylofoni / 木琴</Typography>
+        <Typography>Y - yksi, yrittää / 一，试</Typography>
+        <Typography>Z - zen / 禅</Typography>
         <Typography>Å - Åbo / Turku / 图尔库 (Åbo为其瑞典语称呼) </Typography>
-        <Typography>Ä - ääni, älykäs / sound, intelligent / 声音，智慧</Typography>
-        <Typography>Ö - ötökkä, öljy / bug, oil / 虫子，油</Typography>
+        <Typography>Ä - ääni, älykäs / 声音，智慧</Typography>
+        <Typography>Ö - ötökkä, öljy / 虫子，油</Typography>
       </Box>
     </Box>
   );
