808c1ba9344595a3457c1fb2b6260bee101a5f12
Hope <yagamidai@gmail.com>
2024-02-29 15:22:07 +0800

    Translated into Chinese

--- src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseLongShortVowels.tsx
+++ src/language/chinese/components/navigation/navigationPage/notes/chineseNotes1/ChineseLongShortVowels.tsx
@@ -7,16 +7,16 @@ function ChineseLongShortVowels() {
         Long + Short Vowels/Consanants
       </Typography>
       <Box>
-        <Typography>kina / an argument - Kiina, China</Typography>
-        <Typography>sinä / you, singular - siinä / there</Typography>
-        <Typography>Mali / Mali - maali / paint</Typography>
-        <Typography>katua / to regret - kaatua / to fall</Typography>
-        <Typography>sata / a hundred - sataa / to rain</Typography>
+        <Typography>kina / 一个争论 - Kiina, 中国</Typography>
+        <Typography>sinä / 你 - siinä / 在那里(there)</Typography>
+        <Typography>Mali / 邮件 - maali / 画</Typography>
+        <Typography>katua / 遗憾 - kaatua / 跌落</Typography>
+        <Typography>sata / 百 - sataa / 下雨</Typography>
         <Typography>-----</Typography>
-        <Typography>rapu / crab - rapuu / stair</Typography>
-        <Typography>kuka / who? - kukka / flower</Typography>
-        <Typography>lama / recession - lammas / sheep</Typography>
-        <Typography>mato / worm - matto / carpet = rug</Typography>
+        <Typography>rapu / 螃蟹 - rapuu / 阶梯</Typography>
+        <Typography>kuka / 谁？ - kukka / 花朵</Typography>
+        <Typography>lama / 减退、沮丧 - lammas / 睡觉</Typography>
+        <Typography>mato / 蠕虫 - matto / 地毯</Typography>
         <Typography></Typography>
       </Box>
     </Box>
